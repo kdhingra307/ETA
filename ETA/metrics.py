28424d53f161ccf435685eb4680732cb6576b347
Karan Dhingra <kdhingra307@gmail.com>
2021-04-14 01:44:31 +0530

    melr testing

--- ETA/metrics.py
+++ ETA/metrics.py
@@ -14,8 +14,8 @@ def mse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
     output = ((y_true - y_pred)**2) * mask
     return tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask)
@@ -25,8 +25,8 @@ def mae(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
     output = (tf_maths.abs(y_true - y_pred)) * mask
     return tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask)
@@ -36,8 +36,8 @@ def rmse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
     output = ((y_true - y_pred)**2) * mask
     return tf_maths.sqrt(tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask))
@@ -47,11 +47,10 @@ def mape(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
-    output = tf_maths.abs(y_true - y_pred) / y_true
-    output *= mask
+    output = tf_maths.abs(y_true - y_pred) / tf_maths.abs(y_true)
     output = tf_where(tf_maths.is_nan(output), mask, output)
     output = tf_where(tf_maths.is_inf(output), mask, output)
 
