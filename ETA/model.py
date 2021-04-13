842d7b9f99b153ae9af8e46a6e6e0b3272483554
Karan Dhingra <kdhingra307@gmail.com>
2021-04-13 18:02:24 +0530

    Cleanup

--- ETA/model.py
+++ ETA/model.py
@@ -6,26 +6,6 @@ from ETA import DCGRUBlock, DCGRUCell, config
 import numpy as np
 
 
-
-
-class Encoder(tf_keras.layers.Layer):
-
-    def __init__(self):
-        super(Encoder, self).__init__()
-
-        self.layers = [
-            tf_keras.Sequential([
-                tf_keras.layers.Conv1D(filters=256, kernel_size=3,
-                                       padding="SAME", activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
-                tf_keras.layers.BatchNormalization()])
-        ]*3
-
-    def call(self, x, training=False):
-        for each_layer in self.layers:
-            x += each_layer(x, training)
-        return x
-
-
 class Model(tf_keras.Model):
 
     def __init__(self):
