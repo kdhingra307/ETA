b757808700285733749667b03bdb242d6d06361d
Karan Dhingra <kdhingra307@gmail.com>
2021-04-13 16:38:35 +0530

    Ensure Consistency of loss function with other metrics

--- ETA/metrics.py
+++ ETA/metrics.py
@@ -13,8 +13,8 @@ def mse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
     output = ((y_true - y_pred)**2) * mask
     return tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask)
@@ -24,8 +24,8 @@ def mae(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
     output = (tf_maths.abs(y_true - y_pred)) * mask
     return tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask)
@@ -35,8 +35,8 @@ def rmse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
     output = ((y_true - y_pred)**2) * mask
     return tf_maths.sqrt(tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask))
@@ -46,8 +46,8 @@ def mape(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    y_true = (y_true*std + mean)
-    y_pred = (y_pred*std + mean)
+    # y_true = (y_true*std + mean)
+    # y_pred = (y_pred*std + mean)
 
     output = tf_maths.abs(y_true - y_pred) / y_true
     output = tf_where(tf_maths.is_nan(output), mask, output)
