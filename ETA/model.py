8af7672de07a25c1149bbbe1af5c1f58c26a77e8
Karan Dhingra <kdhingra307@gmail.com>
2021-04-13 18:03:54 +0530

    Approx same parameters

--- ETA/model.py
+++ ETA/model.py
@@ -1,23 +1,6 @@
 import tensorflow.keras as tf_keras
 from ETA import config
 
-class Encoder(tf_keras.layers.Layer):
-
-    def __init__(self):
-        super(Encoder, self).__init__()
-
-        self.layers = [
-            tf_keras.Sequential([
-                tf_keras.layers.Conv1D(filters=256, kernel_size=3,
-                                       padding="SAME", activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
-                tf_keras.layers.BatchNormalization()])
-        ]*3
-
-    def call(self, x, training=False):
-        for each_layer in self.layers:
-            x += each_layer(x, training)
-        return x
-
 
 class Model(tf_keras.Model):
 
@@ -26,22 +9,22 @@ class Model(tf_keras.Model):
         super(Model, self).__init__()
 
         self.network = tf_keras.Sequential([
-            tf_keras.layers.Conv1D(filters=2048, kernel_size=3, padding="SAME",
+            tf_keras.layers.Conv1D(filters=64, kernel_size=3, padding="SAME",
                                    activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
 
-            tf_keras.layers.Conv1D(filters=256, kernel_size=3, padding="SAME",
+            tf_keras.layers.Conv1D(filters=64, kernel_size=3, padding="SAME",
                                    activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
 
             # Encoder(),
 
-            tf_keras.layers.GRU(units=512, return_sequences=True,
+            tf_keras.layers.GRU(units=64, return_sequences=True,
                                 return_state=False),
-            tf_keras.layers.GRU(units=512, return_sequences=True,
+            tf_keras.layers.GRU(units=64, return_sequences=True,
                                 return_state=False),
 
             # Encoder(),
 
-            tf_keras.layers.Conv1D(filters=2048, kernel_size=3, padding="SAME",
+            tf_keras.layers.Conv1D(filters=64, kernel_size=3, padding="SAME",
                                    activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
 
             tf_keras.layers.Conv1D(filters=config.model.num_nodes, kernel_size=3, padding="SAME",
