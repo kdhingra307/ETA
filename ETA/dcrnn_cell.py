22300d36e1208f9136fb4288864792bfe23bd182
Karan Dhingra <kdhingra307@gmail.com>
2021-04-14 00:52:48 +0530

    changed to mean

--- ETA/dcrnn_cell.py
+++ ETA/dcrnn_cell.py
@@ -58,7 +58,7 @@ class DCGRUCell(tf.keras.layers.AbstractRNNCell):
 
     def build(self, inp_shape):
 
-        inpt_features = (inp_shape[-1] + 64)
+        inpt_features = (inp_shape[-1] + 64)*2
         
         kernel_initializer = tf_keras.initializers.GlorotUniform()
         bias_initializer = tf_keras.initializers.Zeros()
