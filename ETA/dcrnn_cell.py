928111c51cb2325e4736749d3dc7618aca7bae4d
Karan Dhingra <kdhingra307@gmail.com>
2021-04-14 16:07:38 +0530

    test

--- ETA/dcrnn_cell.py
+++ ETA/dcrnn_cell.py
@@ -157,8 +157,8 @@ class DCGRUBlock(tf_keras.layers.Layer):
     def decay_teacher_coefficient(self):
         decay_rate = config.model.teacher_decay_rate
 
-        teacher_coeff = decay_rate/ (decay_rate + tf.exp(self.counter/decay_rate))
-        tf.summary.scalar(name="teacher_decay_coefficient", data=teacher_coeff)
+        teacher_coeff = decay_rate/ (decay_rate + tf.exp(self.counter/350))
+        tf.summary.scalar(name="teacher_decay_coefficient", data=teacher_coeff, step=self.counter)
         self.counter += 1
 
         return teacher_coeff
