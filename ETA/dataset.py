90b4384f82f0b4e1a710dde8f91b736bf1227f1f
Karan Dhingra <kdhingra307@gmail.com>
2021-04-14 13:38:46 +0530

    test

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -86,7 +86,7 @@ class sampling:
         
         adj = np.load("{}/{}/metr_adj_matrix.npz".format(
             config.model.working_dir, config.model.static_data_dir))['arr_0'].astype(np.float32)
-        self.adjacency_matrix = [calculate_random_walk_matrix(adj), calculate_random_walk_matrix(adj).T]
+        self.adjacency_matrix = np.stack([calculate_random_walk_matrix(adj), calculate_random_walk_matrix(adj).T], axis=0)
         
         self.n_init = config.model.graph_batch_size
         # self.probab_individ = self.adjacency_matrix**2
