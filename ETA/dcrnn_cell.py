ee661cf3fe47dd078a8fcb2bad66050f8871c034
Karan Dhingra <kdhingra307@gmail.com>
2021-04-24 03:01:28 +0530

    test

--- ETA/dcrnn_cell.py
+++ ETA/dcrnn_cell.py
@@ -132,7 +132,8 @@ class DCGRUBlock(tf_keras.layers.Layer):
         self.cells = dcrnn_cells
         self.num_nodes = num_nodes
         self.steps_to_predict = steps_to_predict
-        self.counter = tf.Variable(config.model.counter_position, dtype=tf.float32, trainable=False)
+        # config.model.counter_position 
+        self.counter = tf.Variable(1, dtype=tf.float32, trainable=False)
         if encode:
             self.block = tf.keras.layers.RNN(self.cells, return_state=True)
         
@@ -150,9 +151,9 @@ class DCGRUBlock(tf_keras.layers.Layer):
         teacher_coeff = decay_rate/ (decay_rate + tf.exp(self.counter/decay_rate))
         tf.summary.scalar(name="teacher_decay_coefficient", data=teacher_coeff)
 
-        self.counter.assign_add(1)
+        # self.counter.assign_add(1)
 
-        return 1.
+        return self.counter
 
     
     @tf.function
