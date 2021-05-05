984a1070c631050142dd2ec0d63a123343599115
Karan Dhingra <kdhingra307@gmail.com>
2021-05-05 22:57:19 +0530

    lstm test

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -8,39 +8,51 @@ mean, std = config.data.mean, config.data.std
 mean_expanded = np.array(mean).reshape([1, 1, -1])
 std_expanded = np.array(std).reshape([1, 1, -1])
 
-def get_data(split_label):
 
+def get_data(split_label):
     def tf_map(file_name):
 
         data = np.load(file_name)
-        x, y = data['x'], data['y'][:, :, 0]
+        x, y = data["x"], data["y"][:, :, 0]
 
         mask = (y > 0) * 1
 
-        y = (y-mean[0])/std[0]
-        x = (x-mean_expanded)/std_expanded
+        y = (y - mean[0]) / std[0]
+        x = (x - mean_expanded) / std_expanded
 
-        x = np.concatenate([x[:, :, 0], x[:, :1, 1]], axis=1).astype(np.float32)
+        x = np.concatenate([x[:, :, 0], x[:, :1, 1]], axis=1).astype(
+            np.float32
+        )
         y = np.stack([y, mask], axis=-1).astype(np.float32)
 
         return x, y
 
-    files = glob("{}/{}/{}/*.npz".format(config.model.working_dir,
-                                         config.data.path_pattern,
-                                         split_label))
+    files = glob(
+        "{}/{}/{}/*.npz".format(
+            config.model.working_dir, config.data.path_pattern, split_label
+        )
+    )
 
     tf_dataset = tf.data.Dataset.from_tensor_slices(files)
-    tf_dataset = tf_dataset.shuffle(config.data.shuffle)
-    tf_dataset = tf_dataset.map(lambda x: tf.numpy_function(tf_map,
-                                                            [x],
-                                                            [tf.float32, tf.float32],
-                                                            name="load_each_file"))
-    tf_dataset = tf_dataset.cache(
-        "{}/cache".format(config.model.working_dir))
-    tf_dataset = tf_dataset.batch(batch_size=config.model.batch_size,
-                                  drop_remainder=True)
 
-    tf_dataset = tf_dataset.map(lambda x, y: (tf.ensure_shape(x, [None, None, config.model.num_nodes+1]), tf.ensure_shape(y, [None, None, config.model.num_nodes, 2])))
+    tf_dataset = tf_dataset.shuffle(config.data.shuffle)
+    tf_dataset = tf_dataset.map(
+        lambda x: tf.numpy_function(
+            tf_map, [x], [tf.float32, tf.float32], name="load_each_file"
+        )
+    )
+
+    tf_dataset = tf_dataset.cache("{}/cache".format(config.model.working_dir))
+    tf_dataset = tf_dataset.batch(
+        batch_size=config.model.batch_size,
+    )
+
+    tf_dataset = tf_dataset.map(
+        lambda x, y: (
+            tf.ensure_shape(x, [None, None, config.model.num_nodes + 1]),
+            tf.ensure_shape(y, [None, None, config.model.num_nodes, 2]),
+        )
+    )
 
     tf_dataset = tf_dataset.prefetch(config.data.prefetch)
 
--- ETA/model.py
+++ ETA/model.py
@@ -1,36 +1,177 @@
 import tensorflow.keras as tf_keras
+import tensorflow.autodiff as tf_diff
+import tensorflow.python.ops.array_ops as tf_array_ops
+from tensorflow.python.keras.engine import data_adapter
+import numpy as np
 from ETA import config
+import tensorflow.python.framework.dtypes as tf_dtype
+from tensorflow import function as tf_function
 
 
 class Model(tf_keras.Model):
-
     def __init__(self):
 
         super(Model, self).__init__()
 
-        self.network = tf_keras.Sequential([
-            tf_keras.layers.Conv1D(filters=64, kernel_size=3, padding="SAME",
-                                   activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
+        self.embedding = tf_keras.Sequential(
+            [
+                tf_keras.layers.Conv1D(
+                    filters=128,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+                tf_keras.layers.BatchNormalization(),
+                tf_keras.layers.Conv1D(
+                    filters=64,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+                tf_keras.layers.BatchNormalization(),
+                tf_keras.layers.Conv1D(
+                    filters=128,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+                tf_keras.layers.BatchNormalization(),
+                tf_keras.layers.Conv1D(
+                    filters=64,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+                tf_keras.layers.BatchNormalization(),
+                tf_keras.layers.Conv1D(
+                    filters=128,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+            ]
+        )
 
-            tf_keras.layers.Conv1D(filters=64, kernel_size=3, padding="SAME",
-                                   activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
+        self.encoder = tf_keras.layers.RNN(
+            tf_keras.layers.StackedRNNCells(
+                [
+                    tf_keras.layers.GRUCell(units=256),
+                    tf_keras.layers.GRUCell(units=128),
+                ]
+            ),
+            return_state=True,
+        )
 
-            # Encoder(),
+        self.decoder = tf_keras.layers.StackedRNNCells(
+            [
+                tf_keras.layers.GRUCell(units=256),
+                tf_keras.layers.GRUCell(units=128),
+            ]
+        )
 
-            tf_keras.layers.GRU(units=64, return_sequences=True,
-                                return_state=False),
-            tf_keras.layers.GRU(units=64, return_sequences=True,
-                                return_state=False),
+        self.post_process = tf_keras.Sequential(
+            [
+                tf_keras.layers.Conv1D(
+                    filters=64,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+                tf_keras.layers.BatchNormalization(),
+                tf_keras.layers.Conv1D(
+                    filters=128,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+                tf_keras.layers.BatchNormalization(),
+                tf_keras.layers.Conv1D(
+                    filters=1,
+                    kernel_size=3,
+                    padding="SAME",
+                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                ),
+            ]
+        )
 
-            # Encoder(),
+    # @tf.function
+    # def decay_teacher_coefficient(self):
+    #     decay_rate = config.model.teacher_decay_rate
 
-            tf_keras.layers.Conv1D(filters=64, kernel_size=3, padding="SAME",
-                                   activation=tf_keras.layers.LeakyReLU(alpha=0.2)),
+    #     teacher_coeff = decay_rate/ (decay_rate + self.counter/128)
+    #     tf.summary.scalar(
+    #         name="teacher_decay_coefficient",
+    #         data=teacher_coeff,
+    #         step=tf.cast(self.counter, tf.int64),
+    #     )
+    #     self.counter.assign_add(1)
 
-            tf_keras.layers.Conv1D(filters=config.model.num_nodes, kernel_size=3, padding="SAME",
-                                   ),
-        ])
+    #     return teacher_coeff
 
-    def call(self, x, training=False):
-        return self.network(x,
-                            training=training)
+    @tf_function
+    def decode(self, state, x_targ=None, init=None):
+
+        if init is None:
+            num_nodes = config.model.num_nodes
+            import tensorflow as tf
+
+            init = tf_array_ops.zeros(
+                [tf_array_ops.shape(state[0])[0], num_nodes, 1],
+                dtype=tf_dtype.float32,
+            )
+
+        tf.print(tf.shape(init))
+
+        num_steps = config.model.steps_to_predict
+        to_return = []
+        if x_targ is None:
+            for i in range(num_steps):
+                init, state = self.decoder(init, states=state)
+                init = self.post_process(init)
+                to_return.append(init)
+            return tf_array_ops.stack(to_return, axis=1)
+        else:
+            for i in range(num_steps):
+                output, state = self.decoder(init, states=state)
+                output = self.post_process(output)
+                to_return.append(output)
+
+                if np.random.rand() > config.model.ttr:
+                    init = output
+                else:
+                    init = x_targ[:, i]
+
+            return tf_array_ops.stack(to_return, axis=1)
+
+    def call(self, x, training=False, y=None):
+
+        embedding = self.embedding(x, training=training)
+        otpt = self.encoder(embedding)
+        encoded = otpt[1:]
+        decoded = self.decode(state=encoded, x_targ=y)
+        return tf_array_ops.squeeze(decoded, axis=-1)
+
+    def train_step(self, data):
+        x, y = data
+
+        with tf_diff.GradientTape() as tape:
+            y_pred = self(x, training=True, y=y[:, :, :, :1])
+            loss = self.compiled_loss(
+                y, y_pred, None, regularization_losses=self.losses
+            )
+
+        self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
+        self.compiled_metrics.update_state(y, y_pred, None)
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, data):
+        x, y = data
+        y_pred = self(x, training=False)
+        # Updates stateful loss metrics.
+        self.compiled_loss(y, y_pred, None, regularization_losses=self.losses)
+        self.compiled_metrics.update_state(y, y_pred, None)
+        return {m.name: m.result() for m in self.metrics}
+
+    def predict_step(self, data):
+        x, _ = data
+        return self(x, training=False)
