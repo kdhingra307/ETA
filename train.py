6ddd007c7ed208780eee1fcee4c1efb4c384d417
Karan Dhingra <kdhingra307@gmail.com>
2021-05-06 02:50:21 +0530

    disable verbose

--- train.py
+++ train.py
@@ -1,15 +1,22 @@
-from ETA import Model, Dataset, loss_function, metrics, config, CheckpointManager
+from ETA import (
+    Model,
+    Dataset,
+    loss_function,
+    metrics,
+    config,
+    CheckpointManager,
+)
 from datetime import datetime
 import tensorflow.keras as tf_keras
 from tensorflow.keras.callbacks import TensorBoard
 from os.path import join as join_directory
 
 
-optimizer = tf_keras.optimizers.Adam(learning_rate=config.training.learning_rate)
+optimizer = tf_keras.optimizers.Adam(
+    learning_rate=config.training.learning_rate
+)
 model = Model()
-model.compile(optimizer=optimizer,
-              loss=loss_function,
-              metrics=metrics)
+model.compile(optimizer=optimizer, loss=loss_function, metrics=metrics)
 
 train_split = config.data.split_prefix.format("train")
 validation_split = config.data.split_prefix.format("val")
@@ -17,13 +24,13 @@ test_split = config.data.split_prefix.format("test")
 
 log_dir = join_directory(
     config.model.working_dir,
-    config.training.log_dir.format(
-        config.model.training_label))
+    config.training.log_dir.format(config.model.training_label),
+)
 
 ckpt_dir = join_directory(
     config.model.working_dir,
-    config.training.ckpt_dir.format(
-        config.model.training_label))
+    config.training.ckpt_dir.format(config.model.training_label),
+)
 
 ckpt_manager = CheckpointManager(optimizer, model, ckpt_dir)
 log_manager = TensorBoard(log_dir=log_dir)
@@ -34,5 +41,6 @@ model.fit(
     Dataset(train_split),
     epochs=config.training.epochs,
     callbacks=[ckpt_manager, log_manager],
-    validation_data=Dataset(validation_split)
+    validation_data=Dataset(validation_split),
+    verbose=2,
 )
