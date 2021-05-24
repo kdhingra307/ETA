30d4147632081360123c0fe4a2517780a2c124cc
Karan Dhingra <kdhingra307@gmail.com>
2021-05-24 14:21:24 +0530

    professor teaching

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -9,39 +9,46 @@ std_expanded = np.array(std).reshape([1, 1, -1])
 
 
 def get_data(split_label):
-
     def tf_map(file_name):
 
         data = np.load(file_name)
-        x, y = data['x'], data['y'][:, :, 0]
+        x, y = data["x"], data["y"][:, :, 0]
 
         mask = (y > 0) * 1
 
-        y = (y-mean[0])/std[0]
-        x = (x-mean_expanded)/std_expanded
+        y = (y - mean[0]) / std[0]
+        x = (x - mean_expanded) / std_expanded
 
         y = np.stack([y, mask], axis=-1).astype(np.float32)
 
         return x.astype(np.float32), y
 
-    files = glob("{}/{}/{}/*.npz".format(config.model.working_dir,
-                                         config.data.path_pattern,
-                                         split_label))
+    files = sorted(
+        glob(
+            "{}/{}/{}/*.npz".format(
+                config.model.working_dir, config.data.path_pattern, split_label
+            )
+        )
+    )
 
     tf_dataset = tf.data.Dataset.from_tensor_slices(files)
-    tf_dataset = tf_dataset.shuffle(config.data.shuffle)
-    tf_dataset = tf_dataset.map(lambda x: tf.numpy_function(tf_map,
-                                                            [x],
-                                                            [tf.float32,
-                                                                tf.float32],
-                                                            name="load_each_file"))
-    tf_dataset = tf_dataset.cache(
-        "{}/cache".format(config.model.working_dir))
+    tf_dataset = tf_dataset.shuffle(config.data.shuffle, seed=1234)
+    tf_dataset = tf_dataset.map(
+        lambda x: tf.numpy_function(
+            tf_map, [x], [tf.float32, tf.float32], name="load_each_file"
+        )
+    )
+    tf_dataset = tf_dataset.cache("{}/cache".format(config.model.working_dir))
 
-    tf_dataset = tf_dataset.map(lambda x, y: (tf.ensure_shape(
-        x, [None, config.model.num_nodes, 2]), tf.ensure_shape(y, [None, config.model.num_nodes, 2])))
-    tf_dataset = tf_dataset.batch(batch_size=config.model.batch_size,
-                                  drop_remainder=True)
+    tf_dataset = tf_dataset.map(
+        lambda x, y: (
+            tf.ensure_shape(x, [None, config.model.num_nodes, 2]),
+            tf.ensure_shape(y, [None, config.model.num_nodes, 2]),
+        )
+    )
+    tf_dataset = tf_dataset.batch(
+        batch_size=config.model.batch_size, drop_remainder=True
+    )
 
     tf_dataset = tf_dataset.prefetch(config.data.prefetch)
 
