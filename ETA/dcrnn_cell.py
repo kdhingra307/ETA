d636fb60fbf406969a4eabd4c91c8ba046a5b9c7
Karan Dhingra <kdhingra307@gmail.com>
2021-06-09 03:35:56 +0530

    Final Test on ETA Dataset

--- ETA/dcrnn_cell.py
+++ ETA/dcrnn_cell.py
@@ -64,7 +64,6 @@ class DCGRUCell(tf.keras.layers.AbstractRNNCell):
                     tf_keras.layers.Dense(
                         units=16, activation=tf_keras.layers.LeakyReLU(0.2)
                     ),
-                    tf_keras.layers.BatchNormalization(),
                     tf_keras.layers.Dense(units=num_proj),
                 ]
             )
