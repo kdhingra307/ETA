dc7e864380215667ff3b13b6bc208518cf59f9df
Karan Dhingra <kdhingra307@gmail.com>
2021-06-26 12:40:33 +0530

    Final Test on ETA Dataset

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -11,7 +11,7 @@ mean_expanded = np.array(mean).reshape([1, 1, -1])
 std_expanded = np.array(std).reshape([1, 1, -1])
 
 adj_mx = np.load(
-    "{}/{}/corr_adj_matrix.npy".format(
+    "{}/{}/corr_adj_matrix.npz.npy".format(
         config.model.working_dir, config.model.static_data_dir
     )
 ).astype(np.float32)
@@ -138,7 +138,7 @@ class rwt_sampling:
 
         self.adj = (
             np.load(
-                "{}/{}/corr_adj_matrix.npy".format(
+                "{}/{}/corr_adj_matrix.npz.npy".format(
                     config.model.working_dir, config.model.static_data_dir
                 )
             ).astype(np.float32)
