30c76f2856750c9ade5e4f2e5274674ef60a2e17
Karan Dhingra <kdhingra307@gmail.com>
2021-06-28 13:40:08 +0530

    Cleaned Dataset for lstm training

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -165,8 +165,8 @@ class rwt_sampling:
 
             chosen_neighbours = (
                 neighbours
-                if len(neighbours) < 32
-                else np.random.choice(neighbours, 32, replace=False)
+                if len(neighbours) < 8
+                else np.random.choice(neighbours, 8, replace=False)
             )
             nodes = np.union1d(
                 nodes,
