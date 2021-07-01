7d1328a1bc8a8e95f9b6c12869ffbe956a5c3548
Karan Dhingra <kdhingra307@gmail.com>
2021-07-02 02:16:10 +0530

    GRUD build

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -11,7 +11,6 @@ std_expanded = np.array(std).reshape([1, 1, -1])
 
 def get_data(split_label):
     def tf_map(file_name):
-        tf.print(file_name)
 
         data = np.load(file_name)
         x, y = data["x"], data["y"][:, :, 0]
@@ -33,7 +32,7 @@ def get_data(split_label):
             dt.append(((1 + dt[-1]) * (1 - x_mask[e])) + x_mask[e])
 
         x1 = np.stack(x1[1:], axis=0)
-        dt = np.stack(dt[1:], axis=0)
+        dt = np.stack(dt[1:], axis=0) / 12
 
         x2 = np.sum(x_mask * x, axis=0) / np.sum(x_mask, axis=0)
 
--- ETA/grud.py
+++ ETA/grud.py
@@ -3,48 +3,34 @@ from tensorflow.python.keras import backend as K
 
 
 class GRUCell(tf.keras.layers.GRUCell):
-    def __init__(self, units, **gru_kwargs):
-        super(GRUCell, self).__init__(units, **gru_kwargs)
-
+    def __init__(self, units):
+        super(GRUCell, self).__init__(units)
+        self.x_prev = tf.keras.layers.Dense(num_variables, name="x_prev")
         self.h_prev = tf.keras.layers.Dense(units, name="h_prev")
-
         self.mask_z = tf.keras.layers.Dense(
             units, name="mask_z", use_bias=False
         )
-
         self.mask_r = tf.keras.layers.Dense(
             units, name="mask_r", use_bias=False
         )
-
         self.mask_h = tf.keras.layers.Dense(
             units, name="mask_h", use_bias=False
         )
 
-    def build(self, input_shape):
-        self.x_prev = tf.keras.layers.Dense(input_shape[-1], name="x_prev")
-        super().build(input_shape)
-
     def call(self, inputs, states, training=False, constants=None):
-        num_variables = inputs.shape[-1] // 4
-
-        x2 = constants[0]
+        x2 = constants[0][:, 0]
 
         x = inputs[:, :num_variables]
         x1 = inputs[:, num_variables : 2 * num_variables]
-
         mask = inputs[:, 2 * num_variables : 3 * num_variables]
         dt = inputs[:, 3 * num_variables :]
-
-        print(x1, dt, mask, x, x2)
 
         mask = mask[:, :1]
         dt = dt[:, :1]
 
         x_prev_mask = self.x_prev(dt)
 
-        x_prev_mask = tf.exp(
-            -1 * tf.clip_by_value(x_prev_mask, 0, tf.float32.max)
-        )
+        x_prev_mask = tf.exp(-1 * tf.clip_by_value(x_prev_mask, 0, np.inf))
 
         inputs = (x * mask) + (
             (1 - mask) * (x_prev_mask * x1 + (1 - x_prev_mask) * x2)
@@ -52,16 +38,9 @@ class GRUCell(tf.keras.layers.GRUCell):
 
         h_prev_mask = self.h_prev(dt)
 
-        h_prev_mask = tf.exp(
-            -1 * tf.clip_by_value(h_prev_mask, 0, tf.float32.max)
-        )
+        h_prev_mask = tf.exp(-1 * tf.clip_by_value(h_prev_mask, 0, np.inf))
 
         h_tm1 = h_prev_mask * states[0]
-
-        dp_mask = self.get_dropout_mask_for_cell(inputs, training, count=3)
-        rec_dp_mask = self.get_recurrent_dropout_mask_for_cell(
-            h_tm1, training, count=3
-        )
 
         if self.use_bias:
             if not self.reset_after:
@@ -69,14 +48,9 @@ class GRUCell(tf.keras.layers.GRUCell):
             else:
                 input_bias, recurrent_bias = tf.unstack(self.bias)
 
-        if 0.0 < self.dropout < 1.0:
-            inputs_z = inputs * dp_mask[0]
-            inputs_r = inputs * dp_mask[1]
-            inputs_h = inputs * dp_mask[2]
-        else:
-            inputs_z = inputs
-            inputs_r = inputs
-            inputs_h = inputs
+        inputs_z = inputs
+        inputs_r = inputs
+        inputs_h = inputs
 
         x_z = K.dot(inputs_z, self.kernel[:, : self.units])
         x_r = K.dot(inputs_r, self.kernel[:, self.units : self.units * 2])
@@ -87,19 +61,15 @@ class GRUCell(tf.keras.layers.GRUCell):
             x_r = K.bias_add(x_r, input_bias[self.units : self.units * 2])
             x_h = K.bias_add(x_h, input_bias[self.units * 2 :])
 
-        if 0.0 < self.recurrent_dropout < 1.0:
-            h_tm1_z = h_tm1 * rec_dp_mask[0]
-            h_tm1_r = h_tm1 * rec_dp_mask[1]
-            h_tm1_h = h_tm1 * rec_dp_mask[2]
-        else:
-            h_tm1_z = h_tm1
-            h_tm1_r = h_tm1
-            h_tm1_h = h_tm1
+        h_tm1_z = h_tm1
+        h_tm1_r = h_tm1
+        h_tm1_h = h_tm1
 
         recurrent_z = K.dot(h_tm1_z, self.recurrent_kernel[:, : self.units])
         recurrent_r = K.dot(
             h_tm1_r, self.recurrent_kernel[:, self.units : self.units * 2]
         )
+
         if self.reset_after and self.use_bias:
             recurrent_z = K.bias_add(recurrent_z, recurrent_bias[: self.units])
             recurrent_r = K.bias_add(
