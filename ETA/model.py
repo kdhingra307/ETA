4ae52915818bf5dab9ba8698fe66516a1a383a95
Karan Dhingra <kdhingra307@gmail.com>
2021-07-02 13:27:50 +0530

    GRUD build

--- ETA/model.py
+++ ETA/model.py
@@ -56,12 +56,12 @@ class Model(tf_keras.Model):
         #     ],
         #     name="embedding",
         # )
-        learnable_cell = GRUDCell(units=64)
-        learnable_cell.build((None, None, 208))
+        # learnable_cell = GRUDCell(units=64)
+        # learnable_cell.build((None, None, 208))
         self.encoder = tf_keras.layers.RNN(
             tf_keras.layers.StackedRNNCells(
                 [
-                    learnable_cell,
+                    tf_keras.layers.GRUCell(units=64),
                     tf_keras.layers.GRUCell(
                         units=64, dropout=0.25, recurrent_dropout=0.25
                     ),
@@ -148,7 +148,8 @@ class Model(tf_keras.Model):
         x, y, x2 = data
         print("x2", x2)
         with tf_diff.GradientTape() as tape:
-            y_pred = self(x, training=True, y=y[:, :, :, :1], constants=x2)
+            # y_pred = self(x, training=True, y=y[:, :, :, :1], constants=x2)
+            y_pred = self(x[:, :, :208], training=True, y=y[:, :, :, :1])
             loss = self.compiled_loss(
                 y, y_pred, None, regularization_losses=self.losses
             )
