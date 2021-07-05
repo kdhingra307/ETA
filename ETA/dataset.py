8e63fadb604063d315595e3069736f960fd5ce51
Karan Dhingra <kdhingra307@gmail.com>
2021-07-05 14:16:44 +0530

    GRUD DCRNN - phase 1

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -64,7 +64,7 @@ def get_data(split_label):
 
     tf_dataset = tf_dataset.map(
         lambda x, y, z: (
-            tf.ensure_shape(x, [None, config.model.num_nodes, 2]),
+            tf.ensure_shape(x, [None, config.model.num_nodes, 8]),
             tf.ensure_shape(y, [None, config.model.num_nodes, 2]),
             tf.ensure_shape(z, [config.model.num_nodes, 2]),
         )
