f559fec1f33043e9c6f16bd8d86990711f2a5bd9
Karan Dhingra <kdhingra307@gmail.com>
2021-07-08 04:00:03 +0530

    ETA LSTM 0.3

--- ETA/dcrnn_cell.py
+++ ETA/dcrnn_cell.py
@@ -56,9 +56,6 @@ class DCGRUCell(tf.keras.layers.AbstractRNNCell):
                         units=64, activation=tf_keras.layers.LeakyReLU(0.2)
                     ),
                     tf_keras.layers.Dense(
-                        units=32, activation=tf_keras.layers.LeakyReLU(0.2)
-                    ),
-                    tf_keras.layers.Dense(
                         units=16, activation=tf_keras.layers.LeakyReLU(0.2)
                     ),
                     tf_keras.layers.Dense(units=num_proj),
