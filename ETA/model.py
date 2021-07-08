833f47a74621092aed9f1065545c2c232af5fef8
Karan Dhingra <kdhingra307@gmail.com>
2021-07-09 02:44:38 +0530

    stp

--- ETA/model.py
+++ ETA/model.py
@@ -40,18 +40,18 @@ class Model(tf_keras.Model):
 
         self.post_process = tf_keras.Sequential(
             [
-                tf_keras.layers.BatchNormalization(),
-                tf_keras.layers.Dropout(0.5),
-                tf_keras.layers.Dense(
-                    units=128,
-                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
-                ),
-                tf_keras.layers.BatchNormalization(),
-                tf_keras.layers.Dropout(0.5),
-                tf_keras.layers.Dense(
-                    units=256,
-                    activation=tf_keras.layers.LeakyReLU(alpha=0.2),
-                ),
+                # tf_keras.layers.BatchNormalization(),
+                # tf_keras.layers.Dropout(0.5),
+                # tf_keras.layers.Dense(
+                #     units=128,
+                #     activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                # ),
+                # tf_keras.layers.BatchNormalization(),
+                # tf_keras.layers.Dropout(0.5),
+                # tf_keras.layers.Dense(
+                #     units=256,
+                #     activation=tf_keras.layers.LeakyReLU(alpha=0.2),
+                # ),
                 tf_keras.layers.Dense(
                     units=config.model.num_nodes, use_bias=False
                 ),
