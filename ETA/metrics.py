0b88c7badb337d2b21d3a74dc4163c2e88405f4e
Karan Dhingra <kdhingra307@gmail.com>
2021-07-08 10:53:14 +0530

    ETA LSTM 0.3

--- ETA/metrics.py
+++ ETA/metrics.py
@@ -21,8 +21,6 @@ def mse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    print(y_pred, y_true, mask)
-
     # y_true = (y_true*std + mean)
     # y_pred = (y_pred*std + mean)
 
