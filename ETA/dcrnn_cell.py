2548c37735202e7d9d96c92be36a53ae353872f6
Karan Dhingra <kdhingra307@gmail.com>
2021-07-10 04:01:25 +0530

    stp

--- ETA/dcrnn_cell.py
+++ ETA/dcrnn_cell.py
@@ -194,7 +194,7 @@ class GSConv(tf_keras.layers.Layer):
     def call(self, x0, support, training=False):
 
         size = 1
-        x = tf.tensordot(support[:size], x0, axes=[1, 1])
+        x = tf.tensordot(support[1:2], x0, axes=[1, 1])
         x = tf.transpose(x, [2, 1, 3, 0])
         x = tf.reshape(
             x, [tf.shape(x0)[0], tf.shape(x0)[1], x0.shape[-1] * size]
