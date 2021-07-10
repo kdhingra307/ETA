f4fc5736ce1b589ffc2f44b9c89af7d61c7b89f4
Karan Dhingra <kdhingra307@gmail.com>
2021-07-11 00:44:11 +0530

    stp

--- ETA/dataset.py
+++ ETA/dataset.py
@@ -70,7 +70,7 @@ def get_data(split_label):
 
         y = np.stack([y, mask], axis=-1).astype(np.float32)
 
-        return x.astype(np.float32), y, x2.astype(np.float32)
+        return x.astype(np.float32), y
 
     files = glob(
         "{}/{}/{}/*.npz".format(
