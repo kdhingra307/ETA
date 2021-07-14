c3acd8811a69a8fa0a5e6ea736f32e2d23d1d8b7
Karan Dhingra <kdhingra307@gmail.com>
2021-07-14 16:11:06 +0530

    stp

--- ETA/__init__.py
+++ ETA/__init__.py
@@ -1,11 +1,11 @@
 from ETA.utils import get_config, CheckpointManager
+
 config = get_config("./config.yaml")
 
-from ETA.metrics import mse, mae, mape, rmse
+from ETA.metrics import mse, mae, mape, rmse, direction
 from ETA.dataset import get_data as Dataset
 from ETA.dcrnn_cell import DCGRUBlock, DCGRUCell
 from ETA.model import Model
 
-metrics = [mse, mae, mape, rmse]
+metrics = [mse, mae, mape, rmse, direction]
 loss_function = mse
-
--- ETA/metrics.py
+++ ETA/metrics.py
@@ -14,44 +14,42 @@ def mse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    # y_true = (y_true*std + mean)
-    # y_pred = (y_pred*std + mean)
-
-    output = ((y_true - y_pred)**2) * mask
-    return tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask)
+    output = ((y_true - y_pred) ** 2) * mask
+    return tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
 
 
 def mae(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    # y_true = (y_true*std + mean)
-    # y_pred = (y_pred*std + mean)
-
     output = (tf_maths.abs(y_true - y_pred)) * mask
-    return tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask)
+    return tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
+
+
+def direction(y_true, y_pred):
+    mask = y_true[:, :, :, 1]
+    y_true = y_true[:, :, :, 0]
+
+    output = y_true * y_pred * mask
+    return tf_maths.reduce_sum(output > 0) / tf_maths.reduce_sum(mask)
 
 
 def rmse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    # y_true = (y_true*std + mean)
-    # y_pred = (y_pred*std + mean)
-
-    output = ((y_true - y_pred)**2) * mask
-    return tf_maths.sqrt(tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask))
+    output = ((y_true - y_pred) ** 2) * mask
+    return tf_maths.sqrt(
+        tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
+    )
 
 
 def mape(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    # y_true = (y_true*std + mean)
-    # y_pred = (y_pred*std + mean)
-
     output = tf_maths.abs(y_true - y_pred) / tf_maths.abs(y_true)
     output = tf_where(tf_maths.is_nan(output), mask, output)
     output = tf_where(tf_maths.is_inf(output), mask, output)
 
-    return tf_maths.reduce_sum(output)/tf_maths.reduce_sum(mask)
+    return tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
