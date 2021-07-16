65834938ab3a1b3ca574f07b4671183b6759b8ab
Karan Dhingra <kdhingra307@gmail.com>
2021-07-16 17:28:57 +0530

    0.5

--- ETA/metrics.py
+++ ETA/metrics.py
@@ -10,6 +10,20 @@ mean = mean[0]
 std = std[0]
 
 
+# def loss(y_true, y_pred):
+#     mask = y_true[:, :, :, 1]
+#     y_true = y_true[:, :, :, 0]
+
+#     y_true = y_true * std + mean
+#     y_pred = y_pred * std + mean
+
+#     y_pred = tf_maths.log(tf_maths.maximum(y_pred, 1e-7) + 1.0)
+#     y_true = tf_maths.log(tf_maths.maximum(y_true, 1e-7) + 1.0)
+
+#     output = ((y_true - y_pred) ** 2) * mask
+#     return tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
+
+
 def loss(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
@@ -17,10 +31,10 @@ def loss(y_true, y_pred):
     y_true = y_true * std + mean
     y_pred = y_pred * std + mean
 
-    y_pred = tf_maths.log(tf_maths.maximum(y_pred, 1e-7) + 1.0)
-    y_true = tf_maths.log(tf_maths.maximum(y_true, 1e-7) + 1.0)
-
     output = ((y_true - y_pred) ** 2) * mask
+
+    output = tf.where(y_true > y_pred, 2 * output, 1 * output)
+
     return tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
 
 
