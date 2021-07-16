47f144ffe8d6f77a553478055cb7456e482b7695
Karan Dhingra <kdhingra307@gmail.com>
2021-07-16 09:59:46 +0530

    0.5

--- ETA/__init__.py
+++ ETA/__init__.py
@@ -2,10 +2,10 @@ from ETA.utils import get_config, CheckpointManager
 
 config = get_config("./config.yaml")
 
-from ETA.metrics import mse, mae, mape, rmse, direction
+from ETA.metrics import mse, mae, mape, rmse, direction, loss
 from ETA.dataset import get_data as Dataset
 from ETA.dcrnn_cell import DCGRUBlock, DCGRUCell
 from ETA.model import Model
 
 metrics = [mse, mae, mape, rmse, direction]
-loss_function = mse
+loss_function = loss
--- ETA/metrics.py
+++ ETA/metrics.py
@@ -10,6 +10,20 @@ mean = mean[0]
 std = std[0]
 
 
+def loss(y_true, y_pred):
+    mask = y_true[:, :, :, 1]
+    y_true = y_true[:, :, :, 0]
+
+    y_true = y_true * std + mean
+    y_pred = y_pred * std + mean
+
+    y_pred = tf_maths.log(tf_maths.maximum(y_pred, 1e-7) + 1.0)
+    y_true = tf_maths.log(tf_maths.maximum(y_true, 1e-7) + 1.0)
+
+    output = ((y_true - y_pred) ** 2) * mask
+    return tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
+
+
 def mse(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
@@ -30,9 +44,9 @@ def direction(y_true, y_pred):
     mask = y_true[:, :, :, 1]
     y_true = y_true[:, :, :, 0]
 
-    output = y_true * y_pred * mask
+    output = (y_true <= y_pred) * mask
     return tf_maths.reduce_sum(
-        tf.cast(output > 0, tf.float32)
+        tf.cast(output, tf.float32)
     ) / tf_maths.reduce_sum(mask)
 
 
