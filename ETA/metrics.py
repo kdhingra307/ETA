1fb922f02b234223e7f63b51271d4d482ec6ef5b
Karan Dhingra <kdhingra307@gmail.com>
2021-07-19 01:22:24 +0530

    0.5

--- ETA/metrics.py
+++ ETA/metrics.py
@@ -20,14 +20,12 @@ def loss(y_true, y_pred):
     y_pred = tf_maths.log(tf_maths.maximum(y_pred, 1e-7) + 1.0)
     y_true = tf_maths.log(tf_maths.maximum(y_true, 1e-7) + 1.0)
 
-    # output = (
-    #     tf.maximum(0.61 * (y_true - y_pred), 0.39 * (y_pred - y_true)) * mask
-    # )
-    output = ((y_true - y_pred) ** 2) * mask
+    output = tf.maximum(0.781 * (y_true - y_pred), 0.625 * (y_pred - y_true))
+    output = (output ** 2) * mask
 
     # output = tf.where(y_true > y_pred, 1.2 * output, 1 * output)
 
-    return tf_maths.reduce_sum(output ** 2) / tf_maths.reduce_sum(mask)
+    return tf_maths.reduce_sum(output) / tf_maths.reduce_sum(mask)
 
 
 def mse(y_true, y_pred):
